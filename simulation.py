1eade08c7c9cf8acf0f34f3b83e31f70417f7df2
Deepti Kannan <kannandeepti@yahoo.com>
2023-10-30 23:23:47 -0400

    CODE: working simulation of multivalent spheres

--- simulation.py
+++ simulation.py
@@ -1,4 +1,4 @@
-""" Simulation of WCA spheres in 3D. """
+""" Simulation of multivalent spheres in 3D. """
 
 from __future__ import absolute_import, division, print_function
 
@@ -44,7 +44,7 @@ def patched_particle_geom(f, R=1):
         raise ValueError("Have not implemented yet")
     return np.array(positions)
 
-def simulate_WCA_spheres(gpuid, N, f, volume_fraction,
+def simulate_patched_spheres(gpuid, N, f, volume_fraction,
                          savepath,
                          collision_rate=0.03,
                          nblocks=1000,
@@ -56,6 +56,8 @@ def simulate_WCA_spheres(gpuid, N, f, volume_fraction,
     ----------
     N : int
         number of spheres
+    f : int
+        valency (number of patches per sphere)
     volume_fraction : float
         volume fraction of spheres in PBC box
 
@@ -63,7 +65,7 @@ def simulate_WCA_spheres(gpuid, N, f, volume_fraction,
     -------
 
     """
-    L = ((N * (0.5)**3) / volume_fraction) ** (1/3)
+    L = ((N * (4/3) * np.pi * (0.5)**3) / volume_fraction) ** (1/3)
     print(L)
     reporter = HDF5Reporter(folder=savepath, max_data_length=100, overwrite=True)
     sim = simulation.Simulation(
@@ -88,8 +90,11 @@ def simulate_WCA_spheres(gpuid, N, f, volume_fraction,
     starting_pos = np.array(starting_pos).reshape(((f+1)*N, 3))
     sim.set_data(starting_pos, center=True)
     sim.set_velocities(v=np.zeros(((f+1)*N, 3)))
+    particle_inds = np.arange(0, (f + 1) * N, 1)
     #indices of larger spheres
     molecule_inds = np.arange(0, (f+1)*N, f+1)
+    #indices of patches
+    patch_inds = np.setdiff1d(particle_inds, molecule_inds)
     #sim.add_force(forces.spherical_confinement(sim, r=L, k=5.0))
     sim.add_force(patched_forces.patched_particle_forcekit(
         sim,
@@ -102,14 +107,19 @@ def simulate_WCA_spheres(gpuid, N, f, volume_fraction,
         },
         angle_force_func=forces.angle_force,
         angle_force_kwargs={
-            'k' : 10.0
+            'k' : 30.0
         },
-        nonbonded_force_func=patched_forces.patched_particle_repulsive_group,
+        patch_attraction_force_func=patched_forces.patch_attraction,
+        patch_attraction_force_kwargs={
+            'attractionEnergy' : 3.0,
+            'attractionRadius' : 0.5
+        },
+        nonbonded_force_func=patched_forces.patched_particle_repulsive,
         nonbonded_force_kwargs={
-            'molecule_inds' : molecule_inds,
-            'trunc' : 5.0,
-            'radiusMult' : 1.0
+            'trunc' : 30.0,
+            'radiusMult' : 1.05
         },
+        exclude_intramolecular=True,
         #patches anyway not in interaciton group, so dont need to create exclusions from bonds
         except_bonds=False
     ))
@@ -118,9 +128,15 @@ def simulate_WCA_spheres(gpuid, N, f, volume_fraction,
     sim.print_stats()
     reporter.dump_data()
 
+def evolve_one_generation(sim, bondStepper, time):
+    """ Integrate for some amount of time and then update bonds."""
+    curtime = sim.state.getTime() / simtk.unit.picosecond
+    sim.integrator.stepTo(curtime + time)
+    curBonds, pastBonds = bondStepper.step(curtime, sim.context)
+
 if __name__ == "__main__":
     tic = time.time()
-    simulate_WCA_spheres(1, 1000, 3, 0.1, "test_patch_geom")
+    simulate_patched_spheres(0, 100, 3, 0.3, "test_patch_attr")
     toc = time.time()
     nsecs = toc - tic
 
--- utils/forces.py
+++ utils/forces.py
@@ -12,8 +12,67 @@ try:
 except Exception:
     import simtk.openmm as openmm
 from polychrom import forces
+from itertools import combinations
 
-def patched_particle_repulsive_group(sim_object, molecule_inds, trunc=5.0, radiusMult=1.0,
+def patch_attraction(
+    sim_object,
+    patch_inds,
+    attractionEnergy=3.0,  # base attraction energy for **all** particles
+    attractionRadius=0.5,
+    name="patch_attraction",
+    exclude_intramolecular=True
+):
+    """
+    Negative of polynomial_repulsive -- makes a smoothed version of
+    a square well potential that is attractive until attraction radius,
+    which is also cutoff of force.
+
+    Parameters
+    ----------
+    f : int
+        Valency of patched particle
+    patch_inds: list of int
+        the list of indices of the "sticky" particles. The sticky particles
+        are attracted to each other with `attractionEnergy`
+    attractionEnergy: float
+        the depth of the attractive part of the potential.
+        E(`repulsionRadius`/2 + `attractionRadius`/2) = `attractionEnergy`
+    attractionRadius: float
+        the maximal range of the attractive part of the potential.
+    exclude_intramolecular : bool
+        Defaults to True. Exclude patches on the same sphere from
+        the attractive potential.
+
+    """
+
+    energy = (
+        "- rshft12 * (rshft2 - 1.0) * ATTRe / emin12 - ATTRe;"
+        "rshft12 = rshft4 * rshft4 * rshft4;"
+        "rshft4 = rshft2 * rshft2;"
+        "rshft2 = rshft * rshft;"
+        "rshft = r / ATTRrad * rmin12;"
+        ""
+    )
+
+    force = openmm.CustomNonbondedForce(energy)
+    force.name = name
+
+    force.setCutoffDistance(attractionRadius * sim_object.conlen)
+    force.addGlobalParameter("ATTRe", attractionEnergy * sim_object.kT)
+    force.addGlobalParameter("ATTRrad", attractionRadius * sim_object.conlen)
+    # Coefficients for x^12*(x*x-1)
+    force.addGlobalParameter("emin12", 46656.0 / 823543.0)
+    force.addGlobalParameter("rmin12", np.sqrt(6.0 / 7.0))
+
+    patch_inds = [int(i) for i in patch_inds]
+    force.addInteractionGroup(patch_inds, patch_inds)
+
+    for i in range(sim_object.N):
+        force.addParticle([])
+
+    return force
+
+def patched_particle_repulsive(sim_object, molecule_inds, trunc=5.0, radiusMult=1.0,
                          name="patched_particle_repulsive"):
     """
     Repulsive potential between spheres where patches on spheres are excluded.
@@ -25,7 +84,8 @@ def patched_particle_repulsive_group(sim_object, molecule_inds, trunc=5.0, radiu
     together with its first derivative at r=1.0.
 
     See the gist below with an example of the potential.
-    https://gist.github.com/mimakaev/0327bf6ffe7057ee0e0625092ec8e318
+    https://gist.github.com/mimakaev/0327bf6ffe7057ee0e06250ls
+    92ec8e318
 
     Parameters
     ----------
@@ -65,70 +125,19 @@ def patched_particle_repulsive_group(sim_object, molecule_inds, trunc=5.0, radiu
 
     return force
 
-def patched_particle_repulsive(sim_object, moleculeIDs, trunc=5.0, radiusMult=1.0,
-                         name="polynomial_repulsive"):
-    """
-    Repulsive potential between spheres where patches on spheres are excluded.
-    This version still computes interactions between all pairs of particles and
-    excludes patches by assigning a per particle parameter "molecule" which
-    signifies if a particle is a sphere or a patch.
-
-    This is a simple polynomial repulsive potential. It has the value
-    of `trunc` at zero, stays flat until 0.6-0.7 and then drops to zero
-    together with its first derivative at r=1.0.
-
-    See the gist below with an example of the potential.
-    https://gist.github.com/mimakaev/0327bf6ffe7057ee0e0625092ec8e318
-
-    Parameters
-    ----------
-    moleculeIDs : array-like (N,)
-        array of 0s and 1s where 1s indicate larger spheres and 0's are patches
-    trunc : float
-        the energy value around r=0
-
-    """
-    radius = sim_object.conlen * radiusMult
-    nbCutOffDist = radius
-    repul_energy = (
-        "min(molecule1, molecule2) * Erep;"
-        "Erep = rsc12 * (rsc2 - 1.0) * REPe / emin12 + REPe;"
-        "rsc12 = rsc4 * rsc4 * rsc4;"
-        "rsc4 = rsc2 * rsc2;"
-        "rsc2 = rsc * rsc;"
-        "rsc = r / REPsigma * rmin12;"
-    )
-
-    force = openmm.CustomNonbondedForce(repul_energy)
-    force.name = name
-
-    force.addGlobalParameter("REPe", trunc * sim_object.kT)
-    force.addGlobalParameter("REPsigma", radius)
-    # Coefficients for x^8*(x*x-1)
-    # force.addGlobalParameter('emin12', 256.0 / 3125.0)
-    # force.addGlobalParameter('rmin12', 2.0 / np.sqrt(5.0))
-    # Coefficients for x^12*(x*x-1)
-    force.addGlobalParameter("emin12", 46656.0 / 823543.0)
-    force.addGlobalParameter("rmin12", np.sqrt(6.0 / 7.0))
-    force.addPerParticleParameter("molecule")
-
-    for i in range(sim_object.N):
-        force.addParticle([float(moleculeIDs[i])])
-
-    force.setCutoffDistance(nbCutOffDist)
-
-    return force
-
 def patched_particle_forcekit(
             sim_object,
-            nchains,
+            N,
             f,
             bond_force_func=forces.harmonic_bonds,
             bond_force_kwargs={"bondWiggleDistance": 0.005, "bondLength": 0.5},
             angle_force_func=forces.angle_force,
             angle_force_kwargs={"k": 0.05},
-            nonbonded_force_func=forces.polynomial_repulsive,
-            nonbonded_force_kwargs={"trunc": 5.0, "radiusMult": 1.0},
+            patch_attraction_force_func=patch_attraction,
+            patch_attraction_force_kwargs={'attractionEnergy' : 3.0,  'attractionRadius' : 0.5},
+            nonbonded_force_func=patched_particle_repulsive,
+            nonbonded_force_kwargs={"trunc": 30.0, "radiusMult": 1.05},
+            exclude_intramolecular=True,
             except_bonds=False
     ):
         """Add bonds to create a topology where a centroid sphere is bonded
@@ -137,12 +146,18 @@ def patched_particle_forcekit(
 
         Assumes every fth particle is the centroid of a patched particle.
 
+        TODO: create exclusions between patches on same particle? this would forbid
+        intra-molecular crosslinking
+
         Parameters
         ----------
-        nchains: int
+        N: int
             Number of patched particles in simulation.
         f : int
             valency of patches.
+        exclude_intramolecular : bool
+            If True then do not calculate patch attraction forces between patches on the same
+            particle. True by default.
         except_bonds : bool
             If True then do not calculate non-bonded forces between the
             particles connected by a bond. False by default.
@@ -154,10 +169,16 @@ def patched_particle_forcekit(
         triplets = []
         centroids = []
 
+        particle_inds = np.arange(0, (f + 1) * N, 1)
+        #indices of larger spheres
+        molecule_inds = np.arange(0, (f + 1) * N, f + 1)
+        #indices of patches
+        patch_inds = np.setdiff1d(particle_inds, molecule_inds)
+
         if f >= 4:
             raise ValueError("Have not implemented valencies larger than 4 yet.")
 
-        for i in range(0, (f+1)*nchains, f+1):
+        for i in range(0, (f+1)*N, f+1):
             centroids.append(i)
             #create bonds between central atom and each of the f sticky residues
             bonds += [(i, i + j) for j in range(1, f+1)]
@@ -184,8 +205,18 @@ def patched_particle_forcekit(
             angle_force_kwargs["theta_0"] = theta
             force_list.append(angle_force_func(sim_object, triplets, **angle_force_kwargs))
 
+        if patch_attraction_force_func is not None:
+            patch_force = patch_attraction_force_func(sim_object, patch_inds,
+                                                          **patch_attraction_force_kwargs)
+            if exclude_intramolecular:
+                # add exclusions for patches on the same particle
+                for i in range(0, len(patch_inds), f):
+                    for pair in combinations(patch_inds[i : i + f], 2):
+                        patch_force.addExclusion(pair[0], pair[1])
+            force_list.append(patch_force)
+
         if nonbonded_force_func is not None:
-            nb_force = nonbonded_force_func(sim_object, **nonbonded_force_kwargs)
+            nb_force = nonbonded_force_func(sim_object, molecule_inds, **nonbonded_force_kwargs)
 
             if except_bonds:
                 exc = list(set([tuple(i) for i in np.sort(np.array(bonds), axis=1)]))
